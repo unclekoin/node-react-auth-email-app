10a826b298bead3d15a2461eefc512d517b82ffb
Pavel Koryakin <unclekoin@gmail.com>
2022-03-09 16:29:28 +0300

    feat: add validation

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,9 +1,13 @@
 const Router = require('express').Router;
 const userController = require('../controllers/user-controller');
+const { body } = require('express-validator');
 
 const router = new Router();
 
-router.post('/registration', userController.registration);
+router.post('/registration',
+  body('email').isEmail(),
+  body('password').isLength({ min: 3, max: 32}),
+  userController.registration);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
@@ -11,3 +15,4 @@ router.get('/refresh', userController.refresh);
 router.get('/users', userController.getUsers);
 
 module.exports = router;
+
